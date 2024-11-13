398e0f2cc84e09c40174dcb8de427510e2a810c2
Rhuan-Messias <messias.ifg@gmail.com>
2024-11-13 17:09:14 -0300

    first project

---
+++ CustomerOrderViewer - Project 1/Models/CustomerOrderDetailModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace CustomerOrderViewer.Models
+{
+    class CustomerOrderDetailModel
+    {
+        //These are the properties of our model
+        public int CustomerOrderId { get; set; }
+        public int CustomerId { get; set; }
+        public int ItemId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ CustomerOrderViewer - Project 1/Program.cs
@@ -0,0 +1,45 @@
+﻿using CustomerOrderViewer.Models;
+using CustomerOrderViewer.Repository;
+using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerOrderViewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // the @ will permit the scapes if exists
+                CustomerOrderDetailCommand customerOrderDetailCommand = new CustomerOrderDetailCommand(@"Data Source=localhost;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+
+                IList<CustomerOrderDetailModel> customerOrderDetailModels = customerOrderDetailCommand.GetList();
+
+                if (customerOrderDetailModels.Any())// check if there's anything in the List
+                {
+                    foreach(CustomerOrderDetailModel customerOrderDetailModel in customerOrderDetailModels)
+                    {
+                        Console.WriteLine("{0}: Fullname: {1} {2} (ID: {3}) - purchased {4} for {5} (Id: {6}",
+                            customerOrderDetailModel.CustomerOrderId,
+                            customerOrderDetailModel.FirstName,
+                            customerOrderDetailModel.LastName,
+                            customerOrderDetailModel.CustomerId,
+                            customerOrderDetailModel.Description,
+                            customerOrderDetailModel.Price,
+                            customerOrderDetailModel.ItemId);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something Went Wrong {0}", e.Message);
+            }
+            
+        }
+    }
+}
